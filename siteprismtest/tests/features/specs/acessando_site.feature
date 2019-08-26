7ef481ac0dea79eb0027fafcff8b7730533daabb
Thaís Martin <thaamartiin@hotmail.com>
2019-08-26 16:17:07 -0300

    Acessando url via SitePrism

---
+++ siteprismtest/tests/features/specs/acessando_site.feature
@@ -0,0 +1,9 @@
+#language: pt
+
+@primeiro_page
+
+Funcionalidade: Acessar primeira page
+
+Cenário: Acessar url com sucesso 
+Quando acesso a url
+Então verifico se estou na pagina inicial
--- siteprismtest/tests/features/suport/env.rb
+++ siteprismtest/tests/features/suport/env.rb
@@ -5,8 +5,12 @@ require 'site_prism'
 #configuração do capybara
 
 Capybara.configure do |config|
-   
+
     config.default_driver = :selenium_chrome
     config.app_host = 'https://automacaocombatista.herokuapp.com'
     config.default_max_wait_time = 5
+end
+
+Capybara.register_driver :selenium_chrome do |app|
+    Capybara::Selenium::Driver.new(app, :browser => :chrome)
 end
